ffd2ca8202f1b1ff8dd9855a621d568012f4d4c7
Mitchell Scott <Mitchellscott@me.com>
2021-10-23 13:56:26 -0500

    too many controllers, need to simplify

--- Models/Expenses/AllExpenses.cs
+++ Models/Expenses/AllExpenses.cs
@@ -9,13 +9,12 @@ namespace CashTrack.Models.Expenses
 {
     public class AllExpenses
     {
-        [JsonIgnore]
         public int Id { get; set; }
         public DateTime PurchaseDate { get; set; }
         public decimal Amount { get; set; }
-        public string Merchant { get; set; }
+        public Merchant Merchant { get; set; }
         public string Notes { get; set; }
         public ExpenseCatagory Catagory { get; set; }
-        public List<string> Tags { get; set; }
+        public List<Tag> Tags { get; set; }
     }
 }
