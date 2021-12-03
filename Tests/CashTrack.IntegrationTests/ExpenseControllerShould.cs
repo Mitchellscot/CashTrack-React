e2c28e1e62a3ef9f4d06adb1735ce28cc2d62be8
Mitchell Scott <Mitchellscott@me.com>
2021-12-02 22:09:07 -0600

    finished adding tests for the expense controller

--- Tests/CashTrack.IntegrationTests/ExpenseControllerShould.cs
+++ Tests/CashTrack.IntegrationTests/ExpenseControllerShould.cs
@@ -24,7 +24,7 @@ namespace CashTrack.IntegrationTests
             _fixture = fixture;
             _output = output;
         }
-        #region SingleId
+        #region SingleExpense
         [Theory]
         [ExpenseIdData]
         public async void ReturnASingleExpense(string id)
@@ -65,6 +65,8 @@ namespace CashTrack.IntegrationTests
                 JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()));
         }
         #endregion
+
+        #region MultipleExpenses
         [Fact]
         public async Task ReturnAllExpenses()
         {
@@ -75,6 +77,209 @@ namespace CashTrack.IntegrationTests
             _output.WriteLine(responseObject.ToString());
             responseObject.TotalPages.ShouldBeGreaterThan(287);
         }
+        [Theory]
+        [InlineData("2016-02-14")]
+        [InlineData("2020-01-01")]
+        [InlineData("2012-01-01")]
+        public async Task ReturnsExpensesFromAGivenDate(string date)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=2&beginDate={date}");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            _output.WriteLine(responseObject.ToString());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+        }
+        [Theory]
+        [InlineData("2016-02-16")]
+        [InlineData("2021-01-01")]
+        [InlineData("2012-04-24")]
+        public async Task ReturnsExpensesForAGivenMonth(string date)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=3&beginDate={date}");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            var testMonth = DateTime.Parse(date).Month;
+            foreach (var exp in expenseList)
+            { 
+                exp.PurchaseDate.Month.ShouldBe(testMonth);
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Theory]
+        [InlineData("2018-02-16")]
+        [InlineData("2014-01-01")]
+        [InlineData("2013-04-24")]
+        public async Task ReturnsExpensesForAGivenQuarter(string date)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=4&beginDate={date}");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            var testYear = DateTime.Parse(date).Year;
+            foreach (var exp in expenseList)
+            {
+                exp.PurchaseDate.Year.ShouldBe(testYear);
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Theory]
+        [InlineData("2019-02-16")]
+        [InlineData("2017-01-01")]
+        [InlineData("2015-04-24")]
+        public async Task ReturnsExpensesForAGivenYear(string date)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=5&beginDate={date}");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            var testYear = DateTime.Parse(date).Year;
+            foreach (var exp in expenseList)
+            {
+                exp.PurchaseDate.Year.ShouldBe(testYear);
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Theory]
+        [InlineData("2012-03-01", "2012-03-14")]
+        [InlineData("2016-11-03", "2021-01-06")]
+        [InlineData("2015-04-24", "2016-04-24")]
+        public async Task ReturnsExpensesForAGivenDateRange(string beginDate, string endDate)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=6&beginDate={beginDate}&endDate={endDate}");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            foreach (var exp in expenseList)
+            {
+                exp.PurchaseDate.ShouldBeInRange(DateTimeOffset.Parse(beginDate), DateTimeOffset.Parse(endDate));
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Fact]
+        public async Task ReturnsExpensesFromLast30Days()
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=7");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            foreach (var exp in expenseList)
+            {
+                exp.PurchaseDate.ShouldBeGreaterThan(DateTimeOffset.Now.AddDays(-31));
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Fact(Skip ="No expenses entered this month...")]
+        public async Task ReturnsExpensesForThisMonth()
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=8");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            var thisMonth = DateTimeOffset.Now.Month;
+            foreach (var exp in expenseList)
+            {
+                exp.PurchaseDate.Month.ShouldBeEquivalentTo(thisMonth);
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Fact]
+        public async Task ReturnsExpensesForThisQuarter()
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=9");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            var thisYear = DateTimeOffset.Now.Year;
+            foreach (var exp in expenseList)
+            {
+                exp.PurchaseDate.Year.ShouldBeEquivalentTo(thisYear);
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Fact]
+        public async Task ReturnsExpensesForThisYear()
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=10");
+            response.EnsureSuccessStatusCode();
+            var responseObject = JsonConvert.DeserializeObject<Expense.Response>(await response.Content.ReadAsStringAsync());
+            responseObject.PageNumber.ShouldBeGreaterThan(0);
+            responseObject.Expenses.Count().ShouldBeGreaterThan(0);
+
+            var expenseList = responseObject.Expenses.ToList();
+            var thisYear = DateTimeOffset.Now.Year;
+            foreach (var exp in expenseList)
+            {
+                exp.PurchaseDate.Year.ShouldBeEquivalentTo(thisYear);
+            }
+            _output.WriteLine(responseObject.ToString());
+        }
+        [Theory]
+        [InlineData("2015-04-24", "2016-04-245")]
+        [InlineData("2016-04-24", "mitchell")]
+        [InlineData("2017-04-24", "2185138642")]
+
+        public async Task ErrorWhenDateRangeSearchDoesntHaveEndDate(string beginDate, string endDate)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=6&beginDate={beginDate}&endDate={endDate}");
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.ShouldContain("EndDate");
+            _output.WriteLine(responseString);
+        }
+
+        [Theory]
+        [InlineData("2016-02-142")]
+        [InlineData("Mitchell")]
+        [InlineData("2185138642")]
+        [EmptyData]
+        public async Task ErrorWhenDateSearchDoesntHaveADate(string date)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=2&beginDate={date}");
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.ShouldContain("BeginDate");
+            _output.WriteLine(responseString);
+        }
+        [Theory]
+        [InlineData("2016-02-142")]
+        [InlineData("Mitchell")]
+        [InlineData("2185138642")]
+        [EmptyData]
+        public async Task ErrorWhenMonthSearchDoesntHaveADate(string date)
+        {
+            var response = await _fixture.Client.GetAsync(path + "/" + $"?dateoptions=3&beginDate={date}");
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.ShouldContain("BeginDate");
+            _output.WriteLine(responseString);
+        }
+
+
+
         [Theory]
         [InlineData("-1")]
         [InlineData("13")]
@@ -150,7 +355,7 @@ namespace CashTrack.IntegrationTests
             responseString.ShouldContain("EndDate");
             _output.WriteLine(responseString);
         }
-
+        #endregion
 
         private void PrintRequestAndResponse(object request, object response)
         {
