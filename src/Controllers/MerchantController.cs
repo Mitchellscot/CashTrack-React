0c8e3aaeb318f1a5e56a1f0c8b67c8f01fe7816e
Mitchell Scott <mitchellscott@me.com>
2022-01-10 21:33:12 -0600

    Added a merchant controller, and a custom exception

---
+++ src/Controllers/MerchantController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using CashTrack.Helpers.Exceptions;
+using CashTrack.Models.MerchantModels;
+using CashTrack.Repositories.MerchantRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace CashTrack.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MerchantController : ControllerBase
+    {
+        private readonly ILogger<MerchantController> _logger;
+        private readonly IMerchantRepository _merchantRepository;
+        private readonly IMapper _mapper;
+
+        public MerchantController(ILogger<MerchantController> logger, IMerchantRepository merchantRepository, IMapper mapper)
+        {
+            _logger = logger;
+            _merchantRepository = merchantRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<ActionResult<MerchantModels.Response>> GetAllMerchants([FromQuery] MerchantModels.Request request)
+        {
+            try
+            {
+                var response = await _merchantRepository.GetMerchantsAsync(request);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"HEY MITCH - ERROR GETTING ALL MERCHANTS {ex.Message}");
+                return BadRequest(new { message = ex.Message.ToString() });
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MerchantModels.Response>> GetAMerchantById(int id)
+        {
+            try
+            {
+                var result = await _merchantRepository.GetMerchantsByIdAsync(id);
+                return Ok(_mapper.Map<MerchantModels.Response>(result));
+            }
+            catch (MerchantNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
