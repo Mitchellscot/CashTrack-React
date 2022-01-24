c91d9d563507931a07b283d3f1f6edb49dc4611a
Mitchell Scott <mitchellscott@me.com>
2022-01-24 15:44:26 -0600

    wrote tests for aggregators and csv parsers

---
+++ Tests/CashTrack.Tests/Aggregators/AggregaterTests.cs
@@ -0,0 +1,65 @@
+﻿using CashTrack.Data;
+using CashTrack.Helpers.Aggregators;
+using CashTrack.Repositories.ExpenseRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CashTrack.Tests.Aggregators
+{
+    public class AggregaterTests
+    {
+        private readonly ExpenseRepository _repo;
+        public AggregaterTests()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Test.json")
+                .Build();
+            var dbOptions = new DbContextOptionsBuilder<AppDbContext>().UseNpgsql(config.GetConnectionString("TestDb")).Options;
+            var context = new AppDbContext(dbOptions, config);
+
+            _repo = new ExpenseRepository(context);
+        }
+        [Fact]
+        public async Task CanAggregateExpenses()
+        {
+            var data = await _repo.GetExpensesAndCategories(x => x.merchantid == 85);
+            var result = data.Aggregate(new ExpenseTotalsAggregator(),
+                (acc, e) => acc.Accumulate(e), acc => acc.Compute());
+            //test data does not contain current month or year
+            result.TotalSpentAllTime.ShouldBe(56170.69m);
+        }
+        [Fact]
+        public async Task CanAggregateExpenseStatistics()
+        {
+            var data = await _repo.GetExpensesAndCategories(x => x.merchantid == 85);
+            var result = data.GroupBy(e => e.purchase_date.Year)
+                .Select(g =>
+                    {
+                        var results = g.Aggregate(new ExpenseStatisticsAggregator(),
+                            (acc, e) => acc.Accumulate(e),
+                            acc => acc.Compute());
+
+                        return new AnnualExpenseStatistics()
+                        {
+                            Year = g.Key,
+                            Average = results.Average,
+                            Min = results.Min,
+                            Max = results.Max,
+                            Total = results.Total,
+                            Count = results.Count
+                        };
+                    }).OrderBy(x => x.Year).ToList();
+            result.First().Year.ShouldBe(2012);
+            result.Where(x => x.Year == 2021).SingleOrDefault()!.Total.ShouldBe(14243.34m);
+        }
+    }
+}
--- Tests/CashTrack.Tests/Services/ExpenseServiceTests.cs
+++ Tests/CashTrack.Tests/Services/ExpenseServiceTests.cs
@@ -13,6 +13,8 @@ using Xunit;
 using Shouldly;
 using CashTrack.Models.ExpenseModels;
 using CashTrack.Models.Common;
+using System.Linq.Expressions;
+using Bogus;
 
 namespace CashTrack.Tests.Services
 {
@@ -22,6 +24,7 @@ namespace CashTrack.Tests.Services
         private readonly IMapper _mapper;
         private ExpenseService _sut;
         private readonly Expenses[] _data;
+        private readonly Faker _faker;
 
         public ExpenseServiceTests()
         {
@@ -30,6 +33,7 @@ namespace CashTrack.Tests.Services
             _mapper = mapperConfig.CreateMapper();
             _sut = new ExpenseService(_repo.Object, _mapper);
             _data = GetData();
+            _faker = new Faker();
         }
         [Fact]
         public async void GetById()
@@ -65,7 +69,95 @@ namespace CashTrack.Tests.Services
             var result = await _sut.GetExpensesByNotesAsync(request);
             result.Expenses.Count().ShouldBe(3);
         }
+        [Theory]
+        [InlineData(DateOptions.All)]
+        [InlineData(DateOptions.SpecificDate)]
+        [InlineData(DateOptions.SpecificMonthAndYear)]
+        [InlineData(DateOptions.SpecificQuarter)]
+        [InlineData(DateOptions.SpecificYear)]
+        [InlineData(DateOptions.DateRange)]
+        [InlineData(DateOptions.Last30Days)]
+        [InlineData(DateOptions.CurrentMonth)]
+        [InlineData(DateOptions.CurrentQuarter)]
+        [InlineData(DateOptions.CurrentYear)]
+        public void GetPredicateWorks(DateOptions option)
+        {
+            var request = new ExpenseModels.Request() with { DateOptions = option };
+            var result = _sut.GetPredicate(request);
+            result.NodeType.ShouldBe(System.Linq.Expressions.ExpressionType.Lambda);
+            result.ShouldNotBeNull();
+        }
+        [Theory]
+        [InlineData("2021-01-05")]
+        [InlineData("2011-02-05")]
+        [InlineData("2012-03-05")]
+        [InlineData("2023-04-05")]
+        [InlineData("2024-05-05")]
+        [InlineData("2015-06-05")]
+        [InlineData("2016-07-05")]
+        [InlineData("2017-08-05")]
+        [InlineData("2019-09-05")]
+        [InlineData("2003-10-05")]
+        [InlineData("2009-11-05")]
+        [InlineData("2005-12-03")]
+        public void GetMonthDatesWorks(string month)
+        {
+            var parsedMonth = DateTimeOffset.Parse(month);
+            var result = _sut.GetMonthDatesFromDate(parsedMonth);
+            var beginingOfMonth = new DateTimeOffset(parsedMonth.Year, parsedMonth.Month, 1, 0, 0, 0, new TimeSpan(0, 0, 0));
 
+            result.startDate.ShouldBe(beginingOfMonth);
+            result.endDate.Day.ShouldBeOneOf(28, 29, 30, 31);
+            result.endDate.Year.ShouldBe(parsedMonth.Year);
+        }
+        [Fact]
+        public void GetYearDatesWorks()
+        {
+            var date = _faker.Date.PastOffset(2021);
+            var result = _sut.GetYearDatesFromDate(date);
+            result.startDate.Year.ShouldBe(date.Year);
+            result.startDate.Day.ShouldBe(1);
+            result.endDate.Day.ShouldBe(31);
+        }
+        [Fact]
+        public void GetQuarterDatesWorks()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var date = _faker.Date.PastOffset(i);
+                var result = _sut.GetQuarterDatesFromDate(date);
+                result.startDate.Month.ShouldBeOneOf(1, 4, 7, 10);
+            }
+        }
+        [Fact]
+        public void GetCurrentYearWorks()
+        {
+            var result = _sut.GetCurrentYear();
+            result.Year.ShouldBe(DateTimeOffset.UtcNow.Year);
+        }
+        [Fact]
+        public void GetCurrentMonthWorks()
+        {
+            var result = _sut.GetCurrentMonth();
+            result.Month.ShouldBe(DateTimeOffset.UtcNow.Month);
+        }
+        [Fact]
+        public void GetCurrentQuarterWorks()
+        {
+            var result = _sut.GetCurrentQuarter();
+            result.Month.ShouldBeOneOf(1, 4, 7, 10);
+        }
+        [Fact]
+        public void GetLastDayOfMonthWorks()
+        {
+            var rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                var date = _faker.Date.PastOffset(rand.Next(1, 2020));
+                var result = _sut.GetLastDayOfMonth(date);
+                result.ShouldBeOneOf(28, 29, 30, 31);
+            }
+        }
         private static Expenses[] GetData()
         {
             return new Expenses[]
