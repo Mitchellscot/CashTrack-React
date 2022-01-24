d02a7801fdcf9b4587b1cbe237ffa2fe085386c6
Mitchell Scott <mitchellscott@me.com>
2022-01-24 16:35:26 -0600

    files are created

---
+++ src/Repositories/MainCategoriesRepository.cs
@@ -0,0 +1,49 @@
+﻿using CashTrack.Data;
+using CashTrack.Data.Entities;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CashTrack.Repositories.MainCategoriesRepository
+{
+    public interface IMainCategoriesRepository : IRepository<MainCategories>
+    {
+
+    }
+    public class MainCategoriesRepository : IMainCategoriesRepository
+    {
+        private readonly AppDbContext _context;
+        public MainCategoriesRepository(AppDbContext dbContext) => (_context) = (dbContext);
+
+        public Task<bool> Create(MainCategories entity)
+        {
+            //Create a check here, if there are 25 main categories, throw an error. That's too many.
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(MainCategories entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MainCategories[]> Find(Expression<Func<MainCategories, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MainCategories> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MainCategories[]> FindWithPagination(Expression<Func<MainCategories, bool>> predicate, int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(MainCategories entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/MainCategoriesService.cs
@@ -0,0 +1,20 @@
+﻿using CashTrack.Data.Entities;
+using CashTrack.Repositories;
+using CashTrack.Repositories.MainCategoriesRepository;
+
+namespace CashTrack.Services.MainCategoriesService
+{
+    public interface IMainCategoriesService
+    {
+
+    }
+    public class MainCategoriesService : IMainCategoriesService
+    {
+        private readonly IMainCategoriesRepository _repo;
+
+        public MainCategoriesService(IMainCategoriesRepository mainCategoryRepository)
+        {
+            _repo = mainCategoryRepository;
+        }
+    }
+}
