63fbec35900b7d917348d358828847d6d9ae78fe
Mitchell Scott <mitchellscott@me.com>
2022-01-27 21:37:14 -0600

    added service tests

--- Tests/CashTrack.Tests/Services/IncomeCategoryServiceTests.cs
+++ Tests/CashTrack.Tests/Services/IncomeCategoryServiceTests.cs
@@ -1,12 +1,12 @@
 ﻿using AutoMapper;
 using CashTrack.Data.Entities;
-using CashTrack.Repositories.IncomeCategoryRepository;
-using CashTrack.Services.IncomeCategoryService;
+using CashTrack.Repositories.IncomeSourceRepository;
+using CashTrack.Services.IncomeSourceService;
 using Moq;
 using System.Linq;
 using Xunit;
 using Shouldly;
-using CashTrack.Models.IncomeCategoryModels;
+using CashTrack.Models.IncomeSourceModels;
 using Bogus;
 using CashTrack.Repositories.SubCategoriesRepository;
 using System.Collections.Generic;
@@ -16,19 +16,19 @@ using System;
 
 namespace CashTrack.Tests.Services
 {
-    public class IncomeCategoryServiceTests
+    public class IncomeSourceServiceTests
     {
-        private readonly Mock<IIncomeCategoryRepository> _repo;
+        private readonly Mock<IIncomeSourceRepository> _repo;
         private readonly IMapper _mapper;
-        private readonly IncomeCategoryService _sut;
-        private IncomeCategories[] _data;
+        private readonly IncomeSourceService _sut;
+        private IncomeSources[] _data;
 
-        public IncomeCategoryServiceTests()
+        public IncomeSourceServiceTests()
         {
-            _repo = new Mock<IIncomeCategoryRepository>();
-            var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile(new IncomeCategoryProfile()));
+            _repo = new Mock<IIncomeSourceRepository>();
+            var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile(new IncomeSourcesProfile()));
             _mapper = mapperConfig.CreateMapper();
-            _sut = new IncomeCategoryService(_repo.Object, _mapper);
+            _sut = new IncomeSourceService(_repo.Object, _mapper);
             _data = GetData();
         }
         [Fact]
@@ -36,67 +36,67 @@ namespace CashTrack.Tests.Services
         {
             _repo.Setup(r => r.FindWithPagination(x => true, 1, 25)).ReturnsAsync(_data);
             _repo.Setup(r => r.GetCount(x => true)).ReturnsAsync(3);
-            var request = new IncomeCategoryRequest();
-            var result = await _sut.GetIncomeCategoriesAsync(request);
+            var request = new IncomeSourceRequest();
+            var result = await _sut.GetIncomeSourcesAsync(request);
             result.ListItems.Count().ShouldBe(3);
             result.TotalCount.ShouldBe(3);
-            result.ListItems.FirstOrDefault()!.Name.ShouldBe("Paycheck");
+            result.ListItems.FirstOrDefault()!.Name.ShouldBe("Persuade");
             result.ListItems.FirstOrDefault()!.Id.ShouldBe(1);
-            result.ListItems.LastOrDefault()!.Name.ShouldBe("government");
+            result.ListItems.LastOrDefault()!.Name.ShouldBe("GrowthZone");
         }
         [Fact]
         public async Task Create()
         {
-            _repo.Setup(x => x.Create(It.IsAny<IncomeCategories>())).ReturnsAsync(true);
-            var request = new AddEditIncomeCategory() { Name = "TEST CREATE" };
-            var result = await _sut.CreateIncomeCategoryAsync(request);
+            _repo.Setup(x => x.Create(It.IsAny<IncomeSources>())).ReturnsAsync(true);
+            var request = new AddEditIncomeSource() { Name = "TEST CREATE" };
+            var result = await _sut.CreateIncomeSourceAsync(request);
             result.Name.ShouldBe("TEST CREATE");
             result.Id.ShouldBe(1);
         }
         [Fact]
         public async Task Update()
         {
-            _repo.Setup(x => x.Update(It.IsAny<IncomeCategories>())).ReturnsAsync(true);
-            var objectToUpdate = new IncomeCategories() { id = 1, category = "TEST CREATE" };
+            _repo.Setup(x => x.Update(It.IsAny<IncomeSources>())).ReturnsAsync(true);
+            var objectToUpdate = new IncomeSources() { id = 1, source = "TEST CREATE" };
             _repo.Setup(x => x.FindById(1)).ReturnsAsync(objectToUpdate);
-            var request = new AddEditIncomeCategory() { Id = 1, Name = "TEST UPDATE" };
-            var result = await _sut.UpdateIncomeCategoryAsync(request);
+            var request = new AddEditIncomeSource() { Id = 1, Name = "TEST UPDATE" };
+            var result = await _sut.UpdateIncomeSourceAsync(request);
             result.ShouldBe(true);
         }
         [Fact]
         public async Task Delete()
         {
-            _repo.Setup(x => x.Delete(It.IsAny<IncomeCategories>())).ReturnsAsync(true);
-            var objectToUpdate = new IncomeCategories() { id = 1, category = "Paycheck" };
+            _repo.Setup(x => x.Delete(It.IsAny<IncomeSources>())).ReturnsAsync(true);
+            var objectToUpdate = new IncomeSources() { id = 1, source = "Persuade" };
             _repo.Setup(x => x.FindById(1)).ReturnsAsync(objectToUpdate);
-            var result = await _sut.DeleteIncomeCategoryAsync(1);
+            var result = await _sut.DeleteIncomeSourceAsync(1);
             result.ShouldBe(true);
         }
-        private static IncomeCategories[] GetData()
+        private static IncomeSources[] GetData()
         {
-            return new IncomeCategories[]
+            return new IncomeSources[]
                 {
-                    new IncomeCategories()
+                    new IncomeSources()
                     {
                         id = 1,
-                        category = "Paycheck",
-                        description = "Money from a paycheck" ,
+                        source = "Persuade",
+                        description = "Money from a persuade" ,
                         in_use = true
 
                     },
-                    new IncomeCategories()
+                    new IncomeSources()
                     {
                         id = 2,
-                        category = "Gifts",
-                        description = "Money from gifts" ,
+                        source = "Capillary",
+                        description = "Money from capillary" ,
                         in_use = true
 
                     },
-                    new IncomeCategories()
+                    new IncomeSources()
                     {
                         id = 3,
-                        category = "government",
-                        description = "Money from gubmint" ,
+                        source = "GrowthZone",
+                        description = "Money from growth zone" ,
                         in_use = false
                     },
                 };
